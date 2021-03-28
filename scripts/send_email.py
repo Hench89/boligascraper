64b5a36f1d3a9cbcfda6d2dffad2de89ffb74189
Henrik Christoffersen <hchristoffersen100@gmail.com>
2021-03-28 12:02:20 +0200

    updated emailing

--- scripts/send_email.py
+++ scripts/send_email.py
@@ -1,6 +1,7 @@
 from reporting.emails import send_ssl_mail, get_email_body
 import os
 import sys
+import traceback
 
 # retrieve environment variables
 try:
@@ -9,26 +10,31 @@ try:
     env_password = os.getenv('MAILPASSWORD')
 except:
     print('Unable to retrieve environment variables')
+    traceback.print_exc()
     sys.exit()
 
 
 # prepare table
 try:
-    body = get_email_body(
-        archive_file_path = "./output/boliga.csv", 
-        market_days_old = 4
-    )
+    archive_file_path = "./output/boliga.csv"
+    market_days_old = 4
+    body = get_email_body(archive_file_path, market_days_old)
 except:
     print('Unable to read and prepare table from archive')
+    traceback.print_exc()
     sys.exit()
 
 # send email
-send_ssl_mail(
-    send_from = env_send_from, 
-    password = env_password,
-    subject = "Boliga Listings",
-    body = body,
-    server = "smtp.gmail.com",
-    port = 465, 
-    send_to = env_send_to
-)
+try:
+    send_ssl_mail(
+        send_from = env_send_from,
+        password = env_password,
+        subject = "Boliga Listings",
+        body = body,
+        server = "smtp.gmail.com",
+        port = 465,
+        send_to = env_send_to
+    )
+except:
+    traceback.print_exc()
+    sys.exit()
--- src/reporting/emails.py
+++ src/reporting/emails.py
@@ -17,15 +17,12 @@ def send_ssl_mail(send_from, password, subject, body, server, port, send_to):
     part2 = MIMEText(body, 'html')
     msg.attach(part2)
 
-    try:
-        server = smtplib.SMTP_SSL('smtp.gmail.com', port)
-        server.ehlo()
-        server.login(send_from, password)
-        server.sendmail(send_from, send_to, msg.as_string())
-        server.close()
-        print('Email sent!')
-    except:
-        print('Something went wrong while trying to send email...')
+    server = smtplib.SMTP_SSL('smtp.gmail.com', port)
+    server.ehlo()
+    server.login(send_from, password)
+    server.sendmail(send_from, send_to, msg.as_string())
+    server.close()
+    print('Email sent!')
 
 
 def get_email_body(file_path: str, get_days: int):
@@ -43,10 +40,10 @@ def get_email_body(file_path: str, get_days: int):
     df['market_days'] = np.round(np.int64(df['market_days']),0)
     df['url'] = "URLSTART" + df['url'] + "URLEND"
     df['gmaps'] = "URLSTART" + df['gmaps'] + "URLEND"
-    df = df[output_columns]
 
     # put together
-    output_columns = ['index', 'address1', 'address2', 'list_price', 'living_area', 'rooms', 'url', 'gmaps', 'market_days']
+    output_columns = ['index', 'address1', 'address2', 'list_price', 'living_area', 'rooms', 'url', 'gmaps', 'station_dist_km', 'market_days']
+    df = df[output_columns]
     html_table = build_table(df, 'red_dark')
     html_table = html_table.replace("URLSTART", "<a href=\"")
     html_table = html_table.replace("URLEND", "\">LINK</a>")
