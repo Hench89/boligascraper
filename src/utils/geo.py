7a70f66cff143d28dbc84f7e9267ff418dfde447
Henrik Christoffersen <hchristoffersen100@gmail.com>
2021-04-07 11:57:51 +0200

    moved gmaps function to load part

--- src/utils/geo.py
+++ src/utils/geo.py
@@ -1,26 +1,42 @@
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 from geopy.exc import GeocoderServiceError
+from utils.numbers import strip_postal_code
 
 def get_dk_geo(address):
 
-    def get(address):
-        try:
-            geolocator = Nominatim(user_agent="myapp")
-            loc = geolocator.geocode(query=address, language='da', country_codes='Denmark')
-            latlon = loc.raw['lat'] + ',' + loc.raw['lon']
-            return latlon
-        except (GeocoderServiceError, AttributeError):
-            return ''
-    
-    address_splitted = address.split(',') if ',' in address else None
+    # fetch postalcode
+    postal_code = strip_postal_code(address)
+    parts = address.split(',')
 
-    latlon = get(address)
-    if latlon == '' and type(address_splitted) == list and len(address_splitted)>2:
-        alt_address = address_splitted[0] + ',' + address_splitted[-1]
-        latlon = get(alt_address)
-        
-    return latlon
+    # deconstruct parts
+    street_parts = [p.strip() for p in parts if postal_code not in p]
+    zip_parts = [p.strip() for p in parts if postal_code in p]
+    city_parts = [z.replace(postal_code, '').strip() for z in zip_parts]
+
+    # setup query dict
+    qdict = {'country' : 'DK'}
+    if len(postal_code)>0:
+        qdict['postalcode'] = postal_code
+    if len(city_parts)>0:
+        qdict['city'] = city_parts[0]
+    if len(street_parts)>0:
+        qdict['street'] = ', '.join(street_parts)
+
+    # try with all
+    geolocator = Nominatim(user_agent="myapp")
+    loc = geolocator.geocode(query=qdict)
+    if loc is not None:
+        return loc.raw['lat'] + ',' + loc.raw['lon']
+
+    # try without city
+    geolocator = Nominatim(user_agent="myapp")
+    loc = geolocator.geocode(query=qdict.pop('city'))
+    if loc is not None:
+        return loc.raw['lat'] + ',' + loc.raw['lon']
+
+    print('.. could not find latlng for address: %s' % address)
+    return None
 
 
 def km_dist(point_a, point_b):
@@ -36,10 +52,8 @@ def get_geo_details(address, stations):
     
     # get latlon from address
     d['latlon'] = get_dk_geo(address)
-    d['gmaps'] = 'https://maps.google.com/?q=' + d['latlon']
-    
-    # calculate distance to nearest s-train station
-    if d['latlon'] != '':
+        
+        # calculate distance to nearest s-train station
         stations_latlng = [str(s['lat']) + ',' + str(s['lon']) for s in stations]
         distances = [km_dist(s, d['latlon']) for s in stations_latlng]
         distances = [d for d in distances if d is not '']
